db5ad04ef004207f5d535df3e26c1b910b849486
ChrisNewRelic <90287042+ChrisNewRelic@users.noreply.github.com>
2021-09-07 16:28:21 -0700

    Update news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 require('dotenv').config()
-throw 42;
 module.exports = async function() {
     try {
       const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
@@ -10,5 +9,5 @@ module.exports = async function() {
     }
 
   };
-
+throw 42;
 
