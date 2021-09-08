5e5d2ab553d1351f0856df202c5053029ec4737c
ChrisNewRelic <90287042+ChrisNewRelic@users.noreply.github.com>
2021-09-08 11:09:36 -0700

    Update news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 require('dotenv').config()
-throw 'error goes here';
 module.exports = async function() {
     try {
       const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
